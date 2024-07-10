bdbd5c569b4b4762103fd274a2575e8e1f16f668
Adilet Abilkhan <abilkhan23@icloud.com>
2024-07-10 18:12:19 +0600

    init

--- src/components/AppDrawer.vue
+++ src/components/AppDrawer.vue
@@ -6,22 +6,32 @@ import AppCartItemList from './AppCartItemList.vue'
 <template>
   <div>
     <div class="fixed top-0 left-0 h-full w-full bg-black bg-opacity-70 z-10"></div>
-    <div class="fixed right-0 top-0 h-full bg-white w-1/4 z-20 p-8">
+    <div
+      class="fixed right-0 top-0 h-full bg-white w-full md:w-1/2 lg:w-1/3 xl:w-1/4 z-20 p-4 sm:p-8 overflow-auto"
+    >
       <AppDrawerHead />
 
       <AppCartItemList />
 
+      <div class="flex flex-col gap-4 my-6">
+        <div class="flex gap-3 text-sm sm:text-base">
+          <span>Total:</span>
+          <div class="flex-1 border-b border-dashed"></div>
+          <b>133100 kzt</b>
+        </div>
 
-      <div>
+        <div class="flex gap-3 text-sm sm:text-base">
+          <span>Tax 15%:</span>
+          <div class="flex-1 border-b border-dashed"></div>
+          <b>155100 kzt</b>
+        </div>
 
-
-            <div class="flex">
-              <span>Total</span>
-              <div class="flex-1 border-b border-dashed"></div>
-              <b>133100 kzt</b>
-            </div>
-
-
+        <button
+          disabled
+          class="bg-blue-900 text-white px-4 py-2 sm:px-8 sm:py-2 rounded-xl disabled:opacity-50 hover:bg-blue-950 transition cursor-pointer"
+        >
+          Checkout
+        </button>
       </div>
     </div>
   </div>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -2,45 +2,53 @@
 
 <template>
   <header
-    class="bg-amber-50 rounded-t-3xl px-8 md:px-36 py-6 flex flex-wrap justify-between items-center border-b border-neutral-900"
+    class="bg-amber-50 rounded-t-3xl px-2 sm:px-4 py-2 sm:py-3 md:px-8 md:py-4 flex flex-wrap justify-between items-center border-b border-neutral-900"
   >
-    <div class="flex items-center mb-4 md:mb-0">
-      <img src="/logo.png" alt="Logo" class="h-11 mr-3" />
+    <div class="flex items-center mb-2 sm:mb-0 flex-shrink-0">
+      <img src="/logo.png" alt="Logo" class="h-9 sm:h-10 mr-2 sm:mr-3" />
       <div>
-        <h1 class="text-lg md:text-xl lg:text-2xl xl:text-3xl font-bold">STOUSTICE</h1>
-        <p class="text-xs md:text-sm">Furniture store</p>
+        <h1 class="text-base sm:text-lg lg:text-xl xl:text-2xl font-bold leading-tight">
+          STOUSTICE
+        </h1>
+        <p class="text-xxs sm:text-xs">Furniture store</p>
       </div>
     </div>
     <nav
-      class="flex flex-col md:flex-row items-center w-full md:w-auto justify-between md:justify-center flex-grow"
+      class="flex flex-col sm:flex-row items-center w-full sm:w-auto justify-between sm:justify-center flex-grow space-y-1 sm:space-y-0"
     >
       <ul
-        class="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-16 text-neutral-900 mb-4 md:mb-0 mx-auto"
+        class="flex flex-col sm:flex-row space-y-1 sm:space-y-0 sm:space-x-3 lg:space-x-4 text-neutral-900 mx-auto"
       >
-        <li><a href="#" class="font-bold text-sm md:text-base">HOME</a></li>
-        <li><a href="#" class="font-bold text-sm md:text-base">COLLECTIONS</a></li>
-        <li><a href="#" class="font-bold text-sm md:text-base">NEW ARRIVALS</a></li>
+        <li><a href="#" class="font-bold text-xxs sm:text-xs md:text-sm lg:text-base">HOME</a></li>
+        <li>
+          <a href="#" class="font-bold text-xxs sm:text-xs md:text-sm lg:text-base">COLLECTIONS</a>
+        </li>
+        <li>
+          <a href="#" class="font-bold text-xxs sm:text-xs md:text-sm lg:text-base">NEW ARRIVALS</a>
+        </li>
       </ul>
-      <div class="flex flex-col md:flex-row items-center">
+      <div class="flex flex-col sm:flex-row items-center space-y-1 sm:space-y-0 md:ml-3 lg:ml-4">
         <input
           type="text"
-          placeholder="Search our collections"
-          class="border border-gray-300 rounded-full px-4 py-1 mr-0 md:mr-6 mb-4 md:mb-0"
+          placeholder="Search collections"
+          class="border border-gray-300 rounded-full px-1 sm:px-2 py-0.5 text-xxs sm:text-xs md:text-sm lg:text-base w-full sm:w-auto"
         />
-        <ul class="flex flex-col md:flex-row items-center md:space-x-6">
-          <li class="flex items-center mb-2 md:mb-0">
-            <img src="/cart.svg" alt="Cart" class="h-7 mr-2" />
-            <b class="text-sm md:text-base text-slate-500 hover:text-black cursor-pointer">0 kzt</b>
+        <ul class="flex flex-col sm:flex-row items-center sm:space-x-2 space-y-1 sm:space-y-0">
+          <li class="flex items-center">
+            <img src="/cart.svg" alt="Cart" class="h-4 sm:h-5 md:h-6 mr-0.5 sm:mr-1" />
+            <b class="text-xxs sm:text-xs md:text-sm text-slate-500 hover:text-black cursor-pointer"
+              >0 kzt</b
+            >
           </li>
-          <li class="flex items-center mb-2 md:mb-0">
-            <img src="/heart.svg" alt="Heart" class="h-7 mr-2" />
-            <b class="text-sm md:text-base text-slate-500 hover:text-black cursor-pointer"
+          <li class="flex items-center">
+            <img src="/heart.svg" alt="Heart" class="h-4 sm:h-5 md:h-6 mr-0.5 sm:mr-1" />
+            <b class="text-xxs sm:text-xs md:text-sm text-slate-500 hover:text-black cursor-pointer"
               >Favorites</b
             >
           </li>
-          <li class="flex items-center mb-2 md:mb-0">
-            <img src="/profile.svg" alt="Profile" class="h-7 mr-2" />
-            <b class="text-sm md:text-base text-slate-500 hover:text-black cursor-pointer"
+          <li class="flex items-center">
+            <img src="/profile.svg" alt="Profile" class="h-4 sm:h-5 md:h-6 mr-0.5 sm:mr-1" />
+            <b class="text-xxs sm:text-xs md:text-sm text-slate-500 hover:text-black cursor-pointer"
               >Profile</b
             >
           </li>
@@ -50,4 +58,13 @@
   </header>
 </template>
 
-<style scoped></style>
+<style scoped>
+header {
+  font-size: calc(0.5rem + 0.25vw);
+}
+@media (max-width: 1024px) {
+  header {
+    font-size: calc(0.5rem + 0.5vw);
+  }
+}
+</style>
