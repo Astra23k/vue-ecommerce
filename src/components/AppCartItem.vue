eb02608e0513bb28f4fdae6e398f9da027671430
Adilet Abilkhan <abilkhan23@icloud.com>
2024-07-10 14:39:43 +0600

    init

--- src/components/AppCartItem.vue
+++ src/components/AppCartItem.vue
@@ -1,12 +1,13 @@
 <template>
-  <div class="border border-slate-200 p-4 rounded-xl">
-    <img src="/furniture/furniture-1.jpg" alt="Furniture" />
-    <div class="flex justify-between">
+  <div class="flex items-center border border-slate-200 p-4 rounded-xl mt-4">
+    <img class="w-16 h-16" src="/furniture/furniture-1.jpg" alt="Furniture" />
+
+    <div class="flex flex-col">
       <p>Furniture</p>
 
-      <div>
+      <div class="flex justify-between mt-2">
         <b>133100 kzt</b>
-        <img src="/close.svg" class="h-9" />
+        <img src="/close.svg" class="h-9 opacity-40 hover:opacity-100" />
       </div>
     </div>
   </div>
